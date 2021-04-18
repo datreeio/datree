6382ebcdfb4b8b085d6c9e8332f03a7d53c29f5a
Dima Brusilovsky <dimabru@gmail.com>
2021-04-18 15:10:59 +0300

    feat: add demo file to .datree on installation

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ curl -L $DOWNLOAD_URL -o $OUTPUT_BASENAME_WITH_POSTFIX
 unzip $OUTPUT_BASENAME_WITH_POSTFIX -d $OUTPUT_BASENAME
 
 cp $OUTPUT_BASENAME/datree /usr/local/bin
+
+rm $OUTPUT_BASENAME_WITH_POSTFIX
+rm -rf $OUTPUT_BASENAME
+
+curl https://get.datree.io/k8s-demo.yaml > ~/.datree/k8s-demo.yaml
