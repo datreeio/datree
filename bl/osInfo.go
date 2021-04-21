e2e24dc02bb11a4ab1c4aecf3588997219158d98
myishay <myishay@gmail.com>
2021-04-21 10:25:35 +0300

    fix: clean evaluator

---
+++ bl/osInfo.go
@@ -0,0 +1,18 @@
+package bl
+
+import "github.com/shirou/gopsutil/host"
+
+type OSInfo struct {
+	OS              string
+	PlatformVersion string
+	KernelVersion   string
+}
+
+func createOSInfo() *OSInfo {
+	infoStat, _ := host.Info()
+	return &OSInfo{
+		OS:              infoStat.OS,
+		KernelVersion:   infoStat.KernelVersion,
+		PlatformVersion: infoStat.PlatformVersion,
+	}
+}
