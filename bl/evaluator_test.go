254194b42dfdd875ab5beb5817a9fe046390f26e
Alex <alex@datree.io>
2021-05-11 18:23:54 +0300

    fix: add infrastructure for multiple validation steps

--- bl/evaluator_test.go
+++ bl/evaluator_test.go
@@ -24,6 +24,11 @@ type mockCliClient struct {
 	mock.Mock
 }
 
+func (m *mockCliClient) CreateEvaluation(createEvaluationRequest cliClient.CreateEvaluationRequest) (int, error) {
+	args := m.Called(createEvaluationRequest)
+	return args.Get(0).(int), args.Error(1)
+}
+
 func (m *mockCliClient) RequestEvaluation(evaluationRequest cliClient.EvaluationRequest) (cliClient.EvaluationResponse, error) {
 	args := m.Called(evaluationRequest)
 	return args.Get(0).(cliClient.EvaluationResponse), args.Error(1)
@@ -50,6 +55,10 @@ type propertiesExtractorMockTestCase struct {
 }
 
 type cliClientMockTestCase struct {
+	createEvaluation struct {
+		EvaluationId int
+		errors       error
+	}
 	requestEvaluation struct {
 		response cliClient.EvaluationResponse
 		errors   error
@@ -59,10 +68,11 @@ type cliClientMockTestCase struct {
 type evaluateTestCase struct {
 	name string
 	args struct {
-		paths             []string
-		cliId             string
-		evaluationConc    int
-		evaluationRequest cliClient.EvaluationRequest
+		paths                   []string
+		cliId                   string
+		evaluationConc          int
+		evaluationRequest       cliClient.EvaluationRequest
+		createEvaluationRequest cliClient.CreateEvaluationRequest
 	}
 	mock struct {
 		propertiesExtractor propertiesExtractorMockTestCase
@@ -77,8 +87,9 @@ type evaluateTestCase struct {
 
 func TestEvaluate(t *testing.T) {
 	tests := []*evaluateTestCase{
-		test_evaluate_failedRequest(),
-		test_evaluate_success(),
+		test_create_evaluation_failedRequest(),
+		//test_evaluate_failedRequest(),
+		//test_evaluate_success(),
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -88,6 +99,7 @@ func TestEvaluate(t *testing.T) {
 
 			propertiesExtractor.On("ReadFilesFromPaths", mock.Anything, mock.Anything).Return(tt.mock.propertiesExtractor.readFilesFromPaths.properties, tt.mock.propertiesExtractor.readFilesFromPaths.filesErrors, tt.mock.propertiesExtractor.readFilesFromPaths.errors)
 			cliClient.On("RequestEvaluation", mock.Anything).Return(tt.mock.cliClient.requestEvaluation.response, tt.mock.cliClient.requestEvaluation.errors)
+			cliClient.On("CreateEvaluation", mock.Anything).Return(tt.mock.cliClient.createEvaluation.EvaluationId, tt.mock.cliClient.createEvaluation.errors)
 
 			evaluator := &Evaluator{
 				propertiesExtractor: propertiesExtractor,
@@ -103,7 +115,10 @@ func TestEvaluate(t *testing.T) {
 			actualResponse, actualFilesErrs, actualErr := evaluator.Evaluate(tt.args.paths, tt.args.cliId, tt.args.evaluationConc, "0.0.1")
 
 			propertiesExtractor.AssertCalled(t, "ReadFilesFromPaths", tt.args.paths, tt.args.evaluationConc)
-			cliClient.AssertCalled(t, "RequestEvaluation", tt.args.evaluationRequest)
+			cliClient.AssertCalled(t, "CreateEvaluation", tt.args.createEvaluationRequest)
+			if tt.mock.cliClient.createEvaluation.errors == nil {
+				cliClient.AssertCalled(t, "RequestEvaluation", tt.args.evaluationRequest)
+			}
 
 			assert.Equal(t, tt.expected.response, actualResponse)
 			assert.Equal(t, tt.expected.fileErrors, actualFilesErrs)
@@ -114,7 +129,6 @@ func TestEvaluate(t *testing.T) {
 
 func test_evaluate_success() *evaluateTestCase {
 	evaluationResponse := cliClient.EvaluationResponse{
-		EvaluationId: 1,
 		Results: []cliClient.EvaluationResult{
 			{
 				Passed: true,
@@ -128,18 +142,29 @@ func test_evaluate_success() *evaluateTestCase {
 			},
 		},
 	}
+	createEvaluationResponse := cliClient.CreateEvaluationResponse{
+		EvaluationId: 432,
+	}
 	return &evaluateTestCase{
 		name: "success",
 		args: struct {
-			paths             []string
-			cliId             string
-			evaluationConc    int
-			evaluationRequest cliClient.EvaluationRequest
+			paths                   []string
+			cliId                   string
+			evaluationConc          int
+			evaluationRequest       cliClient.EvaluationRequest
+			createEvaluationRequest cliClient.CreateEvaluationRequest
 		}{
 			paths:          []string{"path1/path2/file.yaml"},
 			cliId:          "cliId-test",
 			evaluationConc: 1,
 			evaluationRequest: cliClient.EvaluationRequest{
+				EvaluationId: createEvaluationResponse.EvaluationId,
+				Files: []propertiesExtractor.FileProperties{{
+					FileName:       "path1/path2/file.yaml",
+					Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}},
+				}},
+			},
+			createEvaluationRequest: cliClient.CreateEvaluationRequest{
 				CliId: "cliId-test",
 				Metadata: cliClient.Metadata{
 					CliVersion:      "0.0.1",
@@ -147,11 +172,8 @@ func test_evaluate_success() *evaluateTestCase {
 					PlatformVersion: "1.2.3",
 					KernelVersion:   "4.5.6",
 				},
-				Files: []propertiesExtractor.FileProperties{{
-					FileName:       "path1/path2/file.yaml",
-					Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}},
-				}},
-			}},
+			},
+		},
 		mock: struct {
 			propertiesExtractor propertiesExtractorMockTestCase
 			cliClient           cliClientMockTestCase
@@ -178,6 +200,12 @@ func test_evaluate_success() *evaluateTestCase {
 					response: evaluationResponse,
 					errors:   nil,
 				},
+				createEvaluation: struct {
+					EvaluationId int
+					errors       error
+				}{
+					EvaluationId: createEvaluationResponse.EvaluationId,
+				},
 			},
 		},
 		expected: struct {
@@ -199,19 +227,30 @@ func test_evaluate_success() *evaluateTestCase {
 }
 
 func test_evaluate_failedRequest() *evaluateTestCase {
+	createEvaluationResponse := cliClient.CreateEvaluationResponse{
+		EvaluationId: 432,
+	}
 	evaluationResponse := cliClient.EvaluationResponse{}
 	return &evaluateTestCase{
 		name: "fail",
 		args: struct {
-			paths             []string
-			cliId             string
-			evaluationConc    int
-			evaluationRequest cliClient.EvaluationRequest
+			paths                   []string
+			cliId                   string
+			evaluationConc          int
+			evaluationRequest       cliClient.EvaluationRequest
+			createEvaluationRequest cliClient.CreateEvaluationRequest
 		}{
 			paths:          []string{"path1/path2/file.yaml"},
 			cliId:          "cliId-test",
 			evaluationConc: 1,
 			evaluationRequest: cliClient.EvaluationRequest{
+				EvaluationId: createEvaluationResponse.EvaluationId,
+				Files: []propertiesExtractor.FileProperties{{
+					FileName:       "path1/path2/file.yaml",
+					Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}},
+				}},
+			},
+			createEvaluationRequest: cliClient.CreateEvaluationRequest{
 				CliId: "cliId-test",
 				Metadata: cliClient.Metadata{
 					CliVersion:      "0.0.1",
@@ -219,11 +258,8 @@ func test_evaluate_failedRequest() *evaluateTestCase {
 					PlatformVersion: "1.2.3",
 					KernelVersion:   "4.5.6",
 				},
-				Files: []propertiesExtractor.FileProperties{{
-					FileName:       "path1/path2/file.yaml",
-					Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}},
-				}},
-			}},
+			},
+		},
 		mock: struct {
 			propertiesExtractor propertiesExtractorMockTestCase
 			cliClient           cliClientMockTestCase
@@ -249,6 +285,13 @@ func test_evaluate_failedRequest() *evaluateTestCase {
 					response: evaluationResponse,
 					errors:   fmt.Errorf("error"),
 				},
+				createEvaluation: struct {
+					EvaluationId int
+					errors       error
+				}{
+					EvaluationId: createEvaluationResponse.EvaluationId,
+					errors:       nil,
+				},
 			},
 		},
 		expected: struct {
@@ -259,6 +302,83 @@ func test_evaluate_failedRequest() *evaluateTestCase {
 			response:   nil,
 			fileErrors: []propertiesExtractor.FileError{},
 			err:        fmt.Errorf("error"),
+		},
+	}
+}
+
+func test_create_evaluation_failedRequest() *evaluateTestCase {
+	createEvaluationResponse := cliClient.CreateEvaluationResponse{}
+	evaluationResponse := cliClient.EvaluationResponse{}
+	return &evaluateTestCase{
+		name: "fail",
+		args: struct {
+			paths                   []string
+			cliId                   string
+			evaluationConc          int
+			evaluationRequest       cliClient.EvaluationRequest
+			createEvaluationRequest cliClient.CreateEvaluationRequest
+		}{
+			paths:          []string{"path1/path2/file.yaml"},
+			cliId:          "cliId-test",
+			evaluationConc: 1,
+			evaluationRequest: cliClient.EvaluationRequest{
+				EvaluationId: createEvaluationResponse.EvaluationId,
+				Files: []propertiesExtractor.FileProperties{{
+					FileName:       "path1/path2/file.yaml",
+					Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}},
+				}},
+			},
+			createEvaluationRequest: cliClient.CreateEvaluationRequest{
+				CliId: "cliId-test",
+				Metadata: cliClient.Metadata{
+					CliVersion:      "0.0.1",
+					Os:              "darwin",
+					PlatformVersion: "1.2.3",
+					KernelVersion:   "4.5.6",
+				},
+			},
+		},
+		mock: struct {
+			propertiesExtractor propertiesExtractorMockTestCase
+			cliClient           cliClientMockTestCase
+		}{
+			propertiesExtractor: propertiesExtractorMockTestCase{
+				readFilesFromPaths: struct {
+					properties  []*propertiesExtractor.FileProperties
+					filesErrors []propertiesExtractor.FileError
+					errors      []error
+				}{
+					properties: []*propertiesExtractor.FileProperties{{
+						FileName:       "path1/path2/file.yaml",
+						Configurations: []propertiesExtractor.K8sConfiguration{{"apiVersion": "extensions/v1beta1"}}}},
+					filesErrors: []propertiesExtractor.FileError{},
+					errors:      []error{},
+				},
+			},
+			cliClient: cliClientMockTestCase{
+				requestEvaluation: struct {
+					response cliClient.EvaluationResponse
+					errors   error
+				}{
+					response: evaluationResponse,
+					errors:   fmt.Errorf("error"),
+				},
+				createEvaluation: struct {
+					EvaluationId int
+					errors       error
+				}{
+					errors: fmt.Errorf("create evaluation error"),
+				},
+			},
+		},
+		expected: struct {
+			response   *EvaluationResults
+			fileErrors []propertiesExtractor.FileError
+			err        error
+		}{
+			response:   nil,
+			fileErrors: []propertiesExtractor.FileError{},
+			err:        fmt.Errorf("create evaluation error"),
 		},
 	}
 }
