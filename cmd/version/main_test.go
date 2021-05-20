abc86ca9a94695342e4d412bcf7810032549fdfd
Alex <alex@datree.io>
2021-05-20 11:58:58 +0300

    fix: fix version test

--- cmd/version/main_test.go
+++ cmd/version/main_test.go
@@ -1,39 +1,61 @@
 package version
-//
-//import (
-//	"bytes"
-//	"io/ioutil"
-//	"testing"
-//
-//	"github.com/datreeio/datree/bl/messager"
-//	"github.com/stretchr/testify/mock"
-//)
-//
-//type mockMessager struct {
-//	mock.Mock
-//}
-//
-//func (m *mockMessager) LoadVersionMessages(messages chan *messager.VersionMessage, cliVersion string) {
-//	m.Called(messages, cliVersion)
-//}
-//
-//func Test_ExecuteCommand(t *testing.T) {
-//	messager := &mockMessager{}
-//
-//	cmd := New(&VersionCommandContext{
-//		CliVersion: "1.2.3",
-//		Messager:   messager,
-//	})
-//
-//	// TODO: ask yishay wtf
-//	b := bytes.NewBufferString("1.2.3")
-//	cmd.SetOut(b)
-//	cmd.Execute()
-//	out, err := ioutil.ReadAll(b)
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	if string(out) != "1.2.3" {
-//		t.Fatalf("expected \"%s\" got \"%s\"", "1.2.3", string(out))
-//	}
-//}
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/datreeio/datree/bl/messager"
+	"github.com/stretchr/testify/mock"
+)
+
+type printerMock struct {
+	mock.Mock
+}
+
+type mockMessager struct {
+	mock.Mock
+}
+
+func (p *printerMock) PrintMessage(messageText string, messageColor string) {
+	p.Called(messageText, messageColor)
+}
+
+func (m *mockMessager) LoadVersionMessages(messages chan *messager.VersionMessage, cliVersion string) {
+	go func() {
+		messages <- &messager.VersionMessage{
+			CliVersion:   "0.0.1",
+			MessageText:  "message text",
+			MessageColor: "White",
+		}
+
+		close(messages)
+	}()
+
+	m.Called(messages, cliVersion)
+}
+
+func Test_ExecuteCommand(t *testing.T) {
+	messager := &mockMessager{}
+	messager.On("LoadVersionMessages", mock.Anything, mock.Anything).Return(nil)
+
+	printer := &printerMock{}
+	printer.On("PrintMessage", mock.Anything, mock.Anything).Return(nil)
+
+	cmd := New(&VersionCommandContext{
+		CliVersion: "1.2.3",
+		Messager:   messager,
+		Printer: printer,
+	})
+
+	b := bytes.NewBufferString("1.2.3")
+	cmd.SetOut(b)
+	cmd.Execute()
+	out, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(out) != "1.2.3" {
+		t.Fatalf("expected \"%s\" got \"%s\"", "1.2.3", string(out))
+	}
+}
