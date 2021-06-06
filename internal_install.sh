fab739783699aae42a2736a79f00069dfe9aa629
Noaa Barki <noaa@datree.io>
2021-06-06 14:06:58 +0300

    fix: add token

--- internal_install.sh
+++ internal_install.sh
@@ -3,11 +3,10 @@
 
 osName=$(uname -s)
 DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases" | grep -o -m 1 "browser_download_url.*internal_${osName}_x86_64.zip")
-echo $DOWNLOAD_URL
-exit 0
 
 DOWNLOAD_URL=${DOWNLOAD_URL//\"}
 DOWNLOAD_URL=${DOWNLOAD_URL/browser_download_url: /}
+
 
 OUTPUT_BASENAME=datree-latest
 OUTPUT_BASENAME_WITH_POSTFIX=$OUTPUT_BASENAME.zip
@@ -17,24 +16,25 @@ unzip $OUTPUT_BASENAME_WITH_POSTFIX -d $OUTPUT_BASENAME
 
 mkdir -p ~/.datree
 
+TOKEN="internal_$(openssl rand -hex 12)"
 if [[ $osName == "Linux" ]]; then
-
-    CONFIG_FILE=/usr/local/bin/datree/config.yaml
-    if ! [test -f "$CONFIG_FILE"]; then
-        sudo rm -f `ls | grep -v "config.yaml"`
-    else 
-        sudo rm -f /usr/local/bin/datree
-    fi
-
+    # remove all except config.yaml
+    sudo rm -f `ls | grep -v "config.yaml"` 
     # copy but without overwrite
     sudo cp -n $OUTPUT_BASENAME/datree /usr/local/bin
 else
-    rm -f /usr/local/bin/datree
+     # remove all except config.yaml
+    sudo rm -f `ls | grep -v "config.yaml"` 
+
     cp $OUTPUT_BASENAME/datree /usr/local/bin
 fi
 
+$TOKEN >> "config.yaml"
+
 rm $OUTPUT_BASENAME_WITH_POSTFIX
 rm -rf $OUTPUT_BASENAME
+
+
 
 curl https://get.datree.io/k8s-demo.yaml > ~/.datree/k8s-demo.yaml
 
