c5d4f270702cc91de161e25cb16c0939caca1389
Dima Brusilovsky <dimabru@gmail.com>
2021-06-27 13:53:56 +0300

    feat: ignore missing schemas flag

--- bl/validation/main.go
+++ bl/validation/main.go
@@ -1,13 +1,23 @@
 package validation
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type InvalidK8sSchemaError struct {
 	ErrorMessage string
 }
 
 func (e *InvalidK8sSchemaError) Error() string {
-	return fmt.Sprintf("k8s schema validation error: %s\n", e.ErrorMessage)
+	return fmt.Sprintf("k8s schema validation error: %s\n%s", e.ErrorMessage, e.usageSuggestion())
+}
+
+func (e *InvalidK8sSchemaError) usageSuggestion() string {
+	if strings.HasPrefix(e.ErrorMessage, "could not find schema for ") {
+		return "You can skip files with missing schemas instead of failing by using the `--ignore-missing-schemas` flag\n"
+	}
+	return ""
 }
 
 type InvalidYamlError struct {
