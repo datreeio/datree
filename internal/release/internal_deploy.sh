62393635a6c77ea91576959d21aa37130b2871a5
Dima Brusilovsky <dimabru@gmail.com>
2021-07-05 17:11:35 +0300

    fix: temp change release name

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -5,4 +5,4 @@ git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL
 git push --tags
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
-curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
