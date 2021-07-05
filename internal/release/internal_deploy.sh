17f81854a225b82b8738fa82c12eda9cab078edd
Dima Brusilovsky <dimabru@gmail.com>
2021-07-05 17:49:49 +0300

    fix: add echo details

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -5,5 +5,5 @@ git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL
 git push --tags
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
-echo $GORELEASER_VERSION
+echo GORELEASER_VERSION ======== $GORELEASER_VERSION
 curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
