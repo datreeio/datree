026654c787ca0d91b297778fde703839d150e7f9
Dima Brusilovsky <dimabru@gmail.com>
2021-07-05 18:19:40 +0300

    fix: rm-dist

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -5,10 +5,4 @@ git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL
 git push --tags
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
-
-cp ./dist/datree-macos_darwin_amd64/datree ./datree
-rm -rf ./dist
-mkdir -p ./dist/datree-macos_darwin_amd64
-cp ./datree ./dist/datree-macos_darwin_amd64/datree
-
-curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
