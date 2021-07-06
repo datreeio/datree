521ee1a880cf9bfffc9f7d780b8d41d556ef826e
Dima Brusilovsky <dimabru@gmail.com>
2021-07-06 16:01:59 +0300

    fix: remove --rm-dist flag

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -1,3 +1,4 @@
+set -ex
 touch temp
 git add -A
 export DATREE_INTERNAL=0.1.$TRAVIS_BUILD_NUMBER-test-internal
@@ -5,4 +6,4 @@ git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push --tags
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
-curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
