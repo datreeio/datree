ea1c931dffa8e52694ea648702be8a9287db0edc
Dima Brusilovsky <dima@datree.io>
2021-07-06 16:47:26 +0300

    feat: sign macos binary with travis
    * Added ability to sign and notarize cli mac executable using travis during cicd
    
    * Signed release will work in staging and main branches

---
+++ internal/release/deploy.sh
@@ -0,0 +1,23 @@
+set -ex
+if [ $TRAVIS_BRANCH == "main" ]; then 
+  export DATREE_BUILD_VERSION=0.1.$TRAVIS_BUILD_NUMBER; 
+else 
+  export DATREE_BUILD_VERSION=0.1.$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH; 
+fi
+
+export GIT_TAG=$DATREE_BUILD_VERSION
+git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git push --tags
+git stash save --keep-index --include-untracked
+
+# Secure private key
+openssl aes-256-cbc -K $encrypted_2dfcdd1dc486_key -iv $encrypted_2dfcdd1dc486_iv -in DatreeCli.p12.enc -out DatreeCli.p12 -d
+
+security create-keychain -p test buildagent.keychain
+security default-keychain -s buildagent.keychain
+security unlock-keychain -p test buildagent.keychain
+security list-keychains -d user -s buildagent.keychain
+security import DatreeCli.p12 -k buildagent.keychain -P $P12_PASSWORD -T /usr/bin/codesign
+security set-key-partition-list -S "apple-tool:,apple:" -s -k test buildagent.keychain
+security find-identity -v
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
---
+++ internal/release/internal_deploy.sh
@@ -0,0 +1,9 @@
+set -ex
+touch temp
+git add -A
+export DATREE_INTERNAL=0.1.$TRAVIS_BUILD_NUMBER-internal
+git commit -m "release $DATREE_INTERNAL"
+git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git push --tags
+export DATREE_BUILD_VERSION=$DATREE_INTERNAL
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
