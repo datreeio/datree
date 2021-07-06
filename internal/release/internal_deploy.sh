e0345416e3b7a5df40fe32d47f988195744a5b8d
Dima Brusilovsky <dimabru@gmail.com>
2021-07-06 15:53:45 +0300

    fix: define DATREE_INTERNAL

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -1,6 +1,6 @@
 touch temp
 git add -A
-export DATREE_INTERNAL=$DATREE_BUILD_VERSION-internal
+export DATREE_INTERNAL=0.1.$TRAVIS_BUILD_NUMBER-test-internal
 git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push --tags
