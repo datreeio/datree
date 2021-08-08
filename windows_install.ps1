2e59085e0ff2c5ec6ae6e5ef89963822dad884e2
myishay <myishay@gmail.com>
2021-08-08 20:22:03 +0300

    fix: use wbasic parsing

--- windows_install.ps1
+++ windows_install.ps1
@@ -18,13 +18,13 @@ if ((Get-ExecutionPolicy).ToString() -notin $allowedExecutionPolicy) {
     break
 }
 
-$DOWNLOAD_URL = (Invoke-WebRequest -Uri 'https://api.github.com/repos/datreeio/datree/releases/latest' | select-string -Pattern 'https://github.com/datreeio/datree/releases/download/\d+\.\d+\.\d+/datree-cli_\d+\.\d+\.\d+_windows_x86_64.zip').Matches.Value
+$DOWNLOAD_URL = (Invoke-WebRequest -Uri 'https://api.github.com/repos/datreeio/datree/releases/latest' -UseBasicParsing | select-string -Pattern 'https://github.com/datreeio/datree/releases/download/\d+\.\d+\.\d+/datree-cli_\d+\.\d+\.\d+_windows_x86_64.zip').Matches.Value
 $OUTPUT_BASENAME = "datree-latest"
 $OUTPUT_BASENAME_WITH_POSTFIX = "$OUTPUT_BASENAME.zip"
 
 Write-Host 'Installing Datree...'
 Write-Host ''
-Invoke-WebRequest -Uri $DOWNLOAD_URL -OutFile $OUTPUT_BASENAME_WITH_POSTFIX
+Invoke-WebRequest -Uri $DOWNLOAD_URL -OutFile $OUTPUT_BASENAME_WITH_POSTFIX -UseBasicParsing
 Write-Host "[V] Downloaded Datree" -ForegroundColor DarkGreen
 
 Expand-Archive -Path $OUTPUT_BASENAME_WITH_POSTFIX -DestinationPath $OUTPUT_BASENAME -Force | Out-Null
@@ -41,7 +41,7 @@ $dotDatreePath = "$home/.datree"
 mkdir -Force $dotDatreePath | Out-Null
 $k8sDemoPath = Join-Path "$dotDatreePath" "k8s-demo.yaml"
 
-Invoke-WebRequest -Uri "https://get.datree.io/k8s-demo.yaml" -OutFile $k8sDemoPath
+Invoke-WebRequest -Uri "https://get.datree.io/k8s-demo.yaml" -OutFile $k8sDemoPath -UseBasicParsing
 
 Write-Host "[V] Finished Installation" -ForegroundColor DarkGreen
 Write-Host ""
