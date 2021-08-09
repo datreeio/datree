4778d913aaed7fa4dff485754be10bdf92f052ce
myishay <myishay@gmail.com>
2021-08-09 14:41:37 +0300

    feat: simple output

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -48,6 +48,64 @@ type Warning struct {
 	InvalidK8sInfo  InvalidK8sInfo
 }
 
+func (p *Printer) SimplePrintWarnings(warnings []Warning) {
+	for _, warning := range warnings {
+		fmt.Fprintf(out, warning.Title)
+		fmt.Fprintln(out)
+
+		if len(warning.InvalidYamlInfo.ValidationErrors) > 0 {
+			fmt.Fprintf(out, "[X] YAML validation\n")
+			fmt.Fprintln(out)
+			for _, validationError := range warning.InvalidYamlInfo.ValidationErrors {
+				fmt.Fprintf(out, "%v %v\n", "[X]", validationError.Error())
+			}
+			fmt.Fprintln(out)
+
+			fmt.Fprintf(out, "[?] Kubernetes schema validation didn't run for this file\n")
+			fmt.Fprintf(out, "[?] Policy check didn't run for this file\n")
+			fmt.Fprintln(out)
+		} else if len(warning.InvalidK8sInfo.ValidationErrors) > 0 {
+			fmt.Fprintf(out, "[V] YAML validation\n")
+			fmt.Fprintf(out, "[X] Kubernetes schema validation\n")
+			fmt.Fprintln(out)
+
+			for _, validationError := range warning.InvalidK8sInfo.ValidationErrors {
+				fmt.Fprintf(out, "%v %v\n", "[X]", validationError.Error())
+			}
+			fmt.Fprintln(out)
+			fmt.Fprintf(out, "[?] Policy check didn't run for this file\n")
+			fmt.Fprintln(out)
+		} else {
+			fmt.Fprintf(out, "[V] YAML validation\n")
+			fmt.Fprintf(out, "[V] Kubernetes schema validation\n")
+
+			fmt.Fprintln(out)
+			fmt.Fprintf(out, "[X] Policy check\n")
+			fmt.Fprintln(out)
+
+			for _, details := range warning.Details {
+				var occurrencesPostfix string
+				if details.Occurrences > 1 {
+					occurrencesPostfix = "s"
+				} else {
+					occurrencesPostfix = ""
+				}
+				formattedOccurrences := fmt.Sprintf(" [%d occurrence%v]", details.Occurrences, occurrencesPostfix)
+
+				fmt.Fprintf(out, "%v %v %v\n", "[X]", details.Caption, formattedOccurrences)
+				for _, occurrenceDetails := range details.OccurrencesDetails {
+					fmt.Fprintf(out, "    — metadata.name: %v (kind: %v)\n", p.getStringOrNotAvailable(occurrenceDetails.MetadataName), p.getStringOrNotAvailable(occurrenceDetails.Kind))
+				}
+				fmt.Fprintf(out, "%v %v\n", "[*]", details.Suggestion)
+
+				fmt.Fprintln(out)
+			}
+		}
+	}
+
+	fmt.Fprintln(out)
+}
+
 func (p *Printer) PrintWarnings(warnings []Warning) {
 	for _, warning := range warnings {
 		p.printInColor(warning.Title, p.theme.Colors.Yellow)
@@ -132,6 +190,18 @@ type EvaluationSummary struct {
 	PassedPolicyCheckCount    int
 }
 
+func (p *Printer) SimplePrintEvaluationSummary(summary EvaluationSummary, k8sVersion string) {
+	fmt.Fprintf(out, "(Summary)\n")
+	fmt.Fprintln(out)
+
+	fmt.Fprintf(out, "- Passing YAML validation: %v/%v\n", summary.PassedYamlValidationCount, summary.FilesCount)
+	fmt.Fprintln(out)
+	fmt.Fprintf(out, "- Passing Kubernetes (%s) schema validation: %v/%v\n", k8sVersion, summary.PassedK8sValidationCount, summary.FilesCount)
+	fmt.Fprintln(out)
+	fmt.Fprintf(out, "- Passing policy check: %v/%v\n", summary.PassedPolicyCheckCount, summary.FilesCount)
+	fmt.Fprintln(out)
+}
+
 func (p *Printer) PrintEvaluationSummary(summary EvaluationSummary, k8sVersion string) {
 	p.printInColor("(Summary)\n", p.theme.Colors.White)
 	fmt.Fprintln(out)
@@ -142,6 +212,37 @@ func (p *Printer) PrintEvaluationSummary(summary EvaluationSummary, k8sVersion s
 	fmt.Fprintln(out)
 	fmt.Fprintf(out, "- Passing policy check: %v/%v\n", summary.PassedPolicyCheckCount, summary.FilesCount)
 	fmt.Fprintln(out)
+}
+
+func (p *Printer) SimplePrintSummaryTable(summary Summary) {
+	summaryTable := tablewriter.NewWriter(os.Stdout)
+	summaryTable.SetAutoWrapText(false)
+	summaryTable.SetAlignment(tablewriter.ALIGN_LEFT)
+
+	rowIndex := 0
+	plainRowsIndex := 0
+	for i, item := range summary.PlainRows {
+		if item.RowIndex == i {
+			summaryTable.Append([]string{summary.PlainRows[i].LeftCol, summary.PlainRows[i].RightCol})
+			plainRowsIndex++
+			rowIndex++
+		}
+	}
+
+	errorRow := []string{summary.ErrorRow.LeftCol, summary.ErrorRow.RightCol}
+	summaryTable.Append(errorRow)
+	rowIndex++
+
+	successRow := []string{summary.SuccessRow.LeftCol, summary.SuccessRow.RightCol}
+	summaryTable.Append(successRow)
+	rowIndex++
+
+	for plainRowsIndex < len(summary.PlainRows) && summary.PlainRows[plainRowsIndex].RowIndex >= rowIndex {
+		summaryTable.Append([]string{summary.PlainRows[plainRowsIndex].LeftCol, summary.PlainRows[plainRowsIndex].RightCol})
+		rowIndex++
+	}
+
+	summaryTable.Render()
 }
 
 func (p *Printer) PrintSummaryTable(summary Summary) {
