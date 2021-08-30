8d0e31ba6630bba1bf687358cd612cef13dae854
Dima Brusilovsky <dimabru@gmail.com>
2021-08-30 12:40:26 +0300

    fix: typo

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -4,7 +4,7 @@ export DATREE_INTERNAL=$SEMVER_NUMBER-internal
 sed -ie "s/homebrew-datree/homebrew-datree-internal/" .goreleaser.yml
 git add -A
 git commit -m "release $DATREE_INTERNAL"
-git tag $DATREE_INTERNAL -a -m "Generaed internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push --tags
 
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
