63cc6b7d20aedb1caa4d0938a4f8bcb257d497c1
Dima Brusilovsky <dimabru@gmail.com>
2021-08-30 13:05:31 +0300

    feat: bump version

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -5,5 +5,6 @@ export DATREE_INTERNAL=$SEMVER_NUMBER-internal
 git commit -m "release $DATREE_INTERNAL"
 git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push --tags
+
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
 curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
