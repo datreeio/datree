e84358ebb1330152416873e6bcea2195d69cca8a
Dima Brusilovsky <dimabru@gmail.com>
2021-09-02 12:05:52 +0300

    fix: set travis wait for all deployments

--- internal/release/internal_deploy.sh
+++ internal/release/internal_deploy.sh
@@ -6,5 +6,5 @@ git tag $DATREE_INTERNAL -a -m "Generated internal tag from TravisCI for build $
 git push --tags
 
 export DATREE_BUILD_VERSION=$DATREE_INTERNAL
-travis_wait curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
+curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION sh -s -- --rm-dist
 bash ./internal/release/brew_push_formula.sh internal $DATREE_INTERNAL
