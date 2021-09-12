a155ee99acc70403878d838352fd34b9af03e3d7
Dima Brusilovsky <dima@datree.io>
2021-09-12 11:55:57 +0300

    feat: --schema-location flag (#171)
    * feat: support --schema-location flag
    
    * fix: add message to brew install
    
    * fix: spacing in formula message
    
    * fix: add tput init in brew generator

--- cmd/test/main_test.go
+++ cmd/test/main_test.go
@@ -71,7 +71,7 @@ func (kv *K8sValidatorMock) ValidateResources(filesConfigurationsChan chan *extr
 	return args.Get(0).(chan *extractor.FileConfigurations), args.Get(1).(chan *validation.InvalidK8sFile)
 }
 
-func (kv *K8sValidatorMock) InitClient(k8sVersion string, ignoreMissingSchemas bool) {
+func (kv *K8sValidatorMock) InitClient(k8sVersion string, ignoreMissingSchemas bool, schemaLocations []string) {
 }
 
 type PrinterMock struct {
@@ -144,7 +144,7 @@ func TestTestCommand(t *testing.T) {
 	invelidK8sFilesChan := newInvalidK8sFilesChan()
 
 	k8sValidatorMock.On("ValidateResources", mock.Anything, mock.Anything).Return(filesConfigurationsChan, invelidK8sFilesChan, newErrorsChan())
-	k8sValidatorMock.On("InitClient", mock.Anything, mock.Anything).Return()
+	k8sValidatorMock.On("InitClient", mock.Anything, mock.Anything, mock.Anything).Return()
 
 	printerMock := &PrinterMock{}
 	printerMock.On("PrintWarnings", mock.Anything)
