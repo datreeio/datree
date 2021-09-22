bf83f3156f7355cd22c3fc2782e7be2f9cee5622
Yishay Mendelsohn <yishay@datree.io>
2021-09-22 16:53:29 +0300

    fix: patch semver issues (#177)

---
+++ internal/release/define_semver_number.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+latestStagingTag=$(git tag --sort=-version:refname | grep '^0.13.\d\+\-staging' | head -n 1 | grep --only-matching '^0.13.\d\+')
+if [ $TRAVIS_BRANCH == "main" ]; then
+    export SEMVER_NUMBER=$latestStagingTag
+else
+    nextVersion=$(echo $latestStagingTag | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+    export SEMVER_NUMBER=$nextVersion
+fi
