f5b5ab88f787e76d83eea1e5f03a64f3a0534faa
myishay <myishay@gmail.com>
2021-09-23 10:50:12 +0300

    fix: echo semver number

--- internal/release/define_semver_number.sh
+++ internal/release/define_semver_number.sh
@@ -3,8 +3,8 @@ set -ex
 
 latestStagingTag=$(git tag --sort=-version:refname | grep '^0.13.\d\+\-staging' | head -n 1 | grep --only-matching '^0.13.\d\+')
 if [ $TRAVIS_BRANCH == "main" ]; then
-    export SEMVER_NUMBER=$latestStagingTag
+    echo "$latestStagingTag"
 else
     nextVersion=$(echo $latestStagingTag | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-    export SEMVER_NUMBER=$nextVersion
+    echo "$nextVersion"
 fi
