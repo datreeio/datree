c5ac4b637af73c557acb576eeb08c8b943ba2e4b
Nilima Chavan <sanchitac067@gmail.com>
2021-10-05 14:48:14 +0530

    build: fix text format

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ curl -sL $DOWNLOAD_URL -o $OUTPUT_BASENAME_WITH_POSTFIX
 echo -e "\033[32m[V] Downloaded Datree"
 
 if ! unzip >/dev/null 2>&1;then
-    echo -e "\e[1;31m error: unzip command not found \e[0m"
-    echo -e "\e[1;33m install unzip command in your system \e[0m"
+    echo -e "\033[31;1m error: unzip command not found \033[0m"
+    echo -e "\033[33;1m install unzip command in your system \033[0m"
     exit 1
 fi
 
@@ -38,7 +38,7 @@ echo
 
 echo -e "\033[35m Usage: $ datree test ~/.datree/k8s-demo.yaml"
 
-echo -e " Using Helm? => https://hub.datree.io/helm-plugin \e[0m"
+echo -e " Using Helm? => https://hub.datree.io/helm-plugin \033[0m"
 
 tput init
 
