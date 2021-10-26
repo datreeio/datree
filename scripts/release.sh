d0de262bdd84f44be3507ed80e1670175defba73
myishay <myishay@gmail.com>
2021-10-26 12:37:26 +0300

    frefactor: use sign_application script

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,16 +15,7 @@ release_tag="$release_tag-test-yishay" # TODO: remove for creating real producti
 git tag $release_tag -a -m "Generated tag from manual TravisCI for production build $TRAVIS_BUILD_NUMBER"
 git push --tags
 
-# Secure private key
-openssl aes-256-cbc -K $encrypted_2dfcdd1dc486_key -iv $encrypted_2dfcdd1dc486_iv -in DatreeCli.p12.enc -out DatreeCli.p12 -d
-
-security create-keychain -p test buildagent.keychain
-security default-keychain -s buildagent.keychain
-security unlock-keychain -p test buildagent.keychain
-security list-keychains -d user -s buildagent.keychain
-security import DatreeCli.p12 -k buildagent.keychain -P $P12_PASSWORD -T /usr/bin/codesign
-security set-key-partition-list -S "apple-tool:,apple:" -s -k test buildagent.keychain
-security find-identity -v
+bash ./scripts/sign_application.sh
 
 curl -sL https://git.io/goreleaser | VERSION=v$GORELEASER_VERSION bash
 
