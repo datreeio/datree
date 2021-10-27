71d9d63d4fc0483426446fb3101388b50cb2af89
Rushi Panchariya <rushipanchariya19@gmail.com>
2021-10-27 17:23:02 +0530

    feat: fix auto completion test file and remove not require code

--- cmd/completion/main_test.go
+++ cmd/completion/main_test.go
@@ -2,62 +2,59 @@ package completion
 
 import (
 	"bytes"
-	"io/ioutil"
+	"strings"
 	"testing"
 
-	"github.com/datreeio/datree/bl/messager"
-	"github.com/stretchr/testify/mock"
+	"github.com/spf13/cobra"
 )
 
-type printerMock struct {
-	mock.Mock
-}
+func Test_ExecuteCommand(t *testing.T) {
 
-type mockMessager struct {
-	mock.Mock
-}
+	outBuff := new(bytes.Buffer)
+	var tt string
+	cmd := New()
 
-func (p *printerMock) PrintMessage(messageText string, messageColor string) {
-	p.Called(messageText, messageColor)
-}
-
-func (m *mockMessager) LoadVersionMessages(messages chan *messager.VersionMessage, cliVersion string) {
-	go func() {
-		messages <- &messager.VersionMessage{
-			CliVersion:   "0.0.1",
-			MessageText:  "message text",
-			MessageColor: "White",
+	b := bytes.NewBufferString("bash") // repalce bash with zsh, fish, powershell for different shell
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"bash"}) // repalce bash with zsh, fish, powershell for different shell
+	cmd.Run = func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(outBuff)
+			tt = outBuff.String()
+			if !strings.Contains(tt, "# bash completion for completion") {
+				t.Fatalf("Failed: expected \"%s\" got \"%s\"", "# bash completion for completion", tt[:35])
+			} else {
+				t.Logf("Success: expected \"%s\" got \"%s\"", "# bash completion for completion", tt[:35])
+			}
+		case "zsh":
+			cmd.Root().GenBashCompletion(outBuff)
+			tt = outBuff.String()
+			if !strings.Contains(tt, "# zsh completion for completion") {
+				t.Fatalf("Failed: expected \"%s\" got \"%s\"", "# zsh completion for completion", tt[:35])
+			} else {
+				t.Logf("Success: expected \"%s\" got \"%s\"", "# zsh completion for completion", tt[:35])
+			}
+		case "fish":
+			cmd.Root().GenBashCompletion(outBuff)
+			tt = outBuff.String()
+			if !strings.Contains(tt, "# fish completion for completion") {
+				t.Fatalf("Failed: expected \"%s\" got \"%s\"", "# fish completion for completion", tt[:35])
+			} else {
+				t.Logf("Success: expected \"%s\" got \"%s\"", "# fish completion for completion", tt[:35])
+			}
+		case "powershell":
+			cmd.Root().GenBashCompletion(outBuff)
+			tt = outBuff.String()
+			if !strings.Contains(tt, "# powershell completion for completion") {
+				t.Fatalf("Failed: expected \"%s\" got \"%s\"", "# powershell completion for completion", tt[:35])
+			} else {
+				t.Logf("Success: expected \"%s\" got \"%s\"", "# powershell completion for completion", tt[:35])
+			}
+		default:
+			t.Fatalf("Failed: expected \"%s\" got \"%s\"", "[bash|zsh|fish|powershell]", b.String())
 		}
 
-		close(messages)
-	}()
-
-	m.Called(messages, cliVersion)
-}
-
-func Test_ExecuteCommand(t *testing.T) {
-	messager := &mockMessager{}
-	messager.On("LoadVersionMessages", mock.Anything, mock.Anything).Return(nil)
-
-	printer := &printerMock{}
-	printer.On("PrintMessage", mock.Anything, mock.Anything).Return(nil)
-
-	cmd := New(&CompletionCommandContext{
-		CliVersion: "1.2.3",
-		Messager:   messager,
-		Printer:    printer,
-	})
-
-	b := bytes.NewBufferString("bash") // Same for zsh, fish, powershell
-	cmd.SetOut(b)
-	cmd.SetArgs([]string{"bash"}) // Same for zsh, fish, powershell
+	}
 	cmd.Execute()
-	out, err := ioutil.ReadAll(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// repalce bash with zsh, fish, powershell as per var b & SetArgs
-	if string(out) != "bash" {
-		t.Fatalf("expected \"%s\" got \"%s\"", "bash", string(out))
-	}
 }
