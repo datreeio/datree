e6fd9598027bc2a9dde8eb5c80067c4744c417d4
myishay <myishay@gmail.com>
2021-12-16 14:29:29 +0200

    wip

--- scripts/upload_install_scripts.sh
+++ scripts/upload_install_scripts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-aws test-connection
 aws s3 cp windows_install.ps1 s3://get.datree.io/windows_install.ps1 --acl public-read
 aws s3 cp install.sh s3://get.datree.io/install.sh --acl public-read
 
