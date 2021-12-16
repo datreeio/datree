98b1d8349bebd66d25f7e3f1bcfa60fb1dc3723e
Dima Brusilovsky <dimabru@gmail.com>
2021-12-16 15:19:06 +0200

    fix: add ecr credentials

--- scripts/upload_install_scripts.sh
+++ scripts/upload_install_scripts.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -ex
 
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOL
+
 aws configure list
 aws s3 cp windows_install.ps1 s3://get.datree.io/windows_install.ps1 --acl public-read
 aws s3 cp install.sh s3://get.datree.io/install.sh --acl public-read
