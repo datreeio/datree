feec9df55a0f02302b9647e84203f80fdf2a9db1
Dima Brusilovsky <dimabru@gmail.com>
2021-12-29 12:42:29 +0200

    wip: release version locally to github

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -12,13 +12,14 @@ else
     nextVersion=$(echo $latestRcTag | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 fi
 
-export DATREE_BUILD_VERSION=$nextVersion-rc
+export DATREE_BUILD_VERSION=$nextVersion-rc-test-changelog
 echo $DATREE_BUILD_VERSION
 
 git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git push --tags
+git push origin $DATREE_BUILD_VERSION
 
-bash ./scripts/sign_application.sh
-curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
+# bash ./scripts/sign_application.sh
+# curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
+GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging goreleaser --rm-dist
 
-bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
+# bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
