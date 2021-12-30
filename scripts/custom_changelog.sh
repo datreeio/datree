6a72025dd62c945e42d08984cff97cfb103ff61f
shmuelsa <95699772+shmuelsa@users.noreply.github.com>
2021-12-30 15:56:04 +0200

    fix: custom echo run

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -5,3 +5,4 @@ latestRelease=$(git tag --sort=-version:refname | grep "\-rc$" | head -n 1)
 customChangeLog=$(git log --pretty="%h %N %s" --decorate=full ${latestRelease//-rc}..HEAD)
 
 echo $customChangeLog
+
