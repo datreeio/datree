1db71aaf71075eb76095c63e30ce8b47859bc8bd
shmuelsa <95699772+shmuelsa@users.noreply.github.com>
2021-12-30 10:22:12 +0200

    refactor: test commit conv

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -23,7 +23,7 @@ latestRelease=$(git tag --sort=-version:refname | grep "\-rc$" | head -n 1)
 # bash ./scripts/sign_application.sh
 # curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
 GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging goreleaser --rm-dist --debug --release-notes <( echo "";
-    git log --pretty="%h - %s" ${latestRelease//-rc}..HEAD
+    Thank you for all the contributions!  git log --pretty="%h - %N %s" --decorate=full ${latestRelease//-rc}..HEAD
 echo "";)
 
 # bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
