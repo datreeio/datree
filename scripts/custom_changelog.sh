460dc0193032712104888bef7e1ca3674bf4066a
shmuelsa <95699772+shmuelsa@users.noreply.github.com>
2021-12-30 16:23:24 +0200

    fix:spacing

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -2,7 +2,7 @@
 set -ex
 
 latestRelease=$(git tag --sort=-version:refname | grep "\-rc$" | head -n 1)
-customChangeLog=$(git log --pretty="%h %N %s" --decorate=full ${latestRelease//-rc}..HEAD)
+customChangeLog=$(git log --pretty="%h %N %s%n" --decorate=full ${latestRelease//-rc}..HEAD)
 
 echo $customChangeLog
 
