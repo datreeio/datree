1e19e5fb94a1537c734fd5809e8609c0a1dfa0cc
shmuelsa <95699772+shmuelsa@users.noreply.github.com>
2022-01-02 18:15:01 +0200

    fix: fix edgecase custom changelog

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-latestRelease=$(git tag --sort=-version:refname | grep "\-rc$" | head -n 1)
+latestRelease=$(git tag -l --sort=-v:refname | grep -v "\-rc$" | grep -v "pull"  | head -n 1)
 customChangeLog=$(git log --pretty="%h %N %s%n" --decorate=full ${latestRelease//-rc}..HEAD)
 
 echo $customChangeLog
