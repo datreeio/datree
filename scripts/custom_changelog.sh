03e8444847421c01541c3fe2209d85269f3a78d7
shmuelsa <shmuel@datree.io>
2022-01-03 17:35:15 +0200

    fix: move generator changelog to inner script

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -2,6 +2,4 @@
 set -ex
 
 latestRelease=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases/latest" | jq -r '.tag_name' )
-customChangeLog=$(git log --pretty="%h %N %s%n" --no-merges --decorate=full ${latestRelease//-rc}..HEAD)
-
-echo $customChangeLog
+git log --pretty='%h %N %s %n' --no-merges --decorate=full ${latestRelease//-rc}..HEAD > ./changelog.txt
