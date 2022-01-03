752427429be318222837db4732b988feb0f0ceae
shmuelsa <shmuel@datree.io>
2022-01-03 14:49:39 +0200

    f

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ex
 
-release_tag=$RELEASE_VERSION
+# release_tag=$RELEASE_VERSION
 
-git checkout "$release_tag-rc"
+# git checkout "$release_tag-rc"
 
-git tag $release_tag -a -m "Generated tag from manual TravisCI for production build $TRAVIS_BUILD_NUMBER"
-git push origin $release_tag # TODO: check if goreleaser pushes the tag itself (so no need to push here)
+# git tag $release_tag -a -m "Generated tag from manual TravisCI for production build $TRAVIS_BUILD_NUMBER"
+# git push origin $release_tag # TODO: check if goreleaser pushes the tag itself (so no need to push here)
 
-bash ./scripts/sign_application.sh
+# bash ./scripts/sign_application.sh
 
-export DATREE_BUILD_VERSION=$release_tag
-echo $DATREE_BUILD_VERSION
+# export DATREE_BUILD_VERSION=$release_tag
+# echo $DATREE_BUILD_VERSION
 
 bash ./scripts/custom_changelog.sh
 curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=main VERSION=v$GORELEASER_VERSION goreleaser --debug --rm-dist --release-notes='./dist/changelog.txt'
