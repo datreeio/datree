fc034be53c523c096884175b4577756df51cf774
shmuelsa <shmuel@datree.io>
2022-01-03 10:48:09 +0200

    fix: support jq, remove commit messages

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-latestRelease=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases/latest" | jq -r .tag_name )
-customChangeLog=$(git log --pretty="%h %N %s%n" --decorate=full ${latestRelease//-rc}..HEAD)
+latestRelease=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases/latest" | jq -r '.tag_name' )
+customChangeLog=$(git log --pretty="%h %N %s%n" --no-merges --decorate=full ${latestRelease//-rc}..HEAD)
 
 echo $customChangeLog
