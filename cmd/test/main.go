2505eef2316c3e8b93850f7ecc63dc91aa4ea7b4
Noaa Barki <noaabarki@Noaas-MacBook-Pro.local>
2022-02-15 16:03:39 +0200

    feat: change test command to exit with code 2 when miconfigurations were found. This is important for the kubectl plugin

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/datreeio/datree/bl/validation"
 	"github.com/datreeio/datree/pkg/ciContext"
 	"github.com/datreeio/datree/pkg/cliClient"
+	"github.com/pkg/errors"
 
 	"github.com/datreeio/datree/pkg/extractor"
 	"github.com/datreeio/datree/pkg/localConfig"
@@ -89,6 +90,8 @@ type Reader interface {
 type LocalConfig interface {
 	GetLocalConfiguration() (*localConfig.ConfigContent, error)
 }
+
+var InvocationError = errors.New("Invocation error")
 
 type TestCommandContext struct {
 	CliVersion   string
@@ -281,15 +284,15 @@ func test(ctx *TestCommandContext, paths []string, flags TestCommandFlags) error
 		}
 	}
 
-	var invocationFailedErr error = nil
-
 	if err != nil {
-		invocationFailedErr = err
-	} else if validationManager.InvalidYamlFilesCount() > 0 || validationManager.InvalidK8sFilesCount() > 0 || results.EvaluationResults.Summary.TotalFailedRules > 0 {
-		invocationFailedErr = fmt.Errorf("")
+		return err
 	}
 
-	return invocationFailedErr
+	if validationManager.InvalidYamlFilesCount() > 0 || validationManager.InvalidK8sFilesCount() > 0 || results.EvaluationResults.Summary.TotalFailedRules > 0 {
+		return InvocationError
+	}
+
+	return nil
 }
 
 func evaluate(ctx *TestCommandContext, filesPaths []string, flags TestCommandFlags, cliId string) (*ValidationManager, *cliClient.CreateEvaluationResponse, evaluation.ResultType, error) {
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"errors"
 	"os"
 
 	"github.com/datreeio/datree/cmd"
+	"github.com/datreeio/datree/cmd/test"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
+		if errors.Is(err, test.InvocationError) {
+			os.Exit(9)
+		}
 		os.Exit(1)
 	}
 }
