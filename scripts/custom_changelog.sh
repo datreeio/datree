cb56066e10d8047f17d26dbedcd2b939095f5f3b
Roy Hadad <roy@datree.io>
2022-03-09 11:23:49 +0200

    ci: add echo in custom_changlog script for debugging

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
 
-latestRelease=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases/latest" | jq -r '.tag_name' )
+latestCliReleaseFromGithub=$(curl --silent "https://api.github.com/repos/datreeio/datree/releases/latest")
+echo $latestCliReleaseFromGithub
+latestRelease=$(echo $latestCliReleaseFromGithub | jq -r '.tag_name' )
 git log --pretty='%h %N %s %n' --no-merges --decorate=full ${latestRelease//-rc}..HEAD > ./changelog.txt
