ca2a6e3ce79be4f15162c37a1bee5126a4f4d7fd
myishay <myishay@gmail.com>
2022-03-16 12:47:13 +0200

    fix: remove debugging log from custom_changelog

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -2,6 +2,6 @@
 set -ex
 
 latestCliReleaseFromGithub=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/datreeio/datree/releases/latest")
-echo $latestCliReleaseFromGithub
+# echo $latestCliReleaseFromGithub
 latestRelease=$(echo $latestCliReleaseFromGithub | jq -r '.tag_name' )
 git log --pretty='%h %N %s %n' --no-merges --decorate=full ${latestRelease//-rc}..HEAD > ./changelog.txt
