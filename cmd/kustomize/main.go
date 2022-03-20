bbb4aab0def2f1d6c3152cd5938a99dbf71c8fdd
myishay <myishay@gmail.com>
2022-03-20 17:51:48 +0200

    fix: update cmd name

--- cmd/kustomize/main.go
+++ cmd/kustomize/main.go
@@ -95,7 +95,7 @@ func New(testCtx *test.TestCommandContext, kustomizeCtx *KustomizeContext) *cobr
 
 	kustomizeCommand := &cobra.Command{
 		Use:   "kustomize",
-		Short: "Render kustomize configs and run a policy check against them",
+		Short: "Render configs of kustomization.yaml file and run a policy check against them",
 	}
 
 	kustomizeCommand.AddCommand(kustomizeTestCommand)
