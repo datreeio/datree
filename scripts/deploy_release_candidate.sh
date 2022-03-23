f2abbeb5ca26611ba871715593caab20aff2e5df
Tzlil Swimmer <tzlilswimmer@Tzlils-MacBook-Pro.local>
2022-03-23 14:20:42 +0200

    feat: bump to version 1.1.0

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -2,7 +2,7 @@
 set -ex
 
 MAJOR_VERSION=1
-MINOR_VERSION=0
+MINOR_VERSION=1
 
 latestRcTag=$(git tag --sort=-version:refname | grep "^${MAJOR_VERSION}.${MINOR_VERSION}.\d\+\-rc" | head -n 1 | grep --only-matching "^${MAJOR_VERSION}.${MINOR_VERSION}.\d\+" || true)
 
