db4eedee7206c1944ffbf7dcaed7a7735651f38b
Roy Hadad <roy@datree.io>
2022-05-10 15:25:54 +0300

    test: add more test cases

--- pkg/policy/validatePoliciesYaml_test.go
+++ pkg/policy/validatePoliciesYaml_test.go
@@ -14,10 +14,26 @@ func Test_customRulesNull(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+//go:embed validatePoliciesYamlFixtures/missingCustomRules.yaml
+var missingCustomRules string
+
+func Test_missingCustomRules(t *testing.T) {
+	err := validatePoliciesYaml([]byte(missingCustomRules), "./validatePoliciesYamlFixtures/missingCustomRules.yaml")
+	assert.Nil(t, err)
+}
+
 //go:embed validatePoliciesYamlFixtures/missingApiVersion.yaml
 var missingApiVersion string
 
 func Test_missingApiVersion(t *testing.T) {
 	err := validatePoliciesYaml([]byte(missingApiVersion), "./validatePoliciesYamlFixtures/missingApiVersion.yaml")
 	assert.EqualError(t, err, "found errors in policies file ./validatePoliciesYamlFixtures/missingApiVersion.yaml:\n(root): missing properties: 'apiVersion'")
+}
+
+//go:embed validatePoliciesYamlFixtures/missingPolicyName.yaml
+var missingPolicyName string
+
+func Test_missingPolicyName(t *testing.T) {
+	err := validatePoliciesYaml([]byte(missingPolicyName), "./validatePoliciesYamlFixtures/missingPolicyName.yaml")
+	assert.EqualError(t, err, "found errors in policies file ./validatePoliciesYamlFixtures/missingPolicyName.yaml:\n(root)/policies/0: missing properties: 'name'")
 }
