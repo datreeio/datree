42c5a5b871d914f318aa9aa26167a1dc2626415e
Roy Hadad <roy@datree.io>
2022-05-10 15:13:33 +0300

    test: add test case for missing api verison

--- pkg/policy/validatePoliciesYaml_test.go
+++ pkg/policy/validatePoliciesYaml_test.go
@@ -9,7 +9,15 @@ import (
 //go:embed validatePoliciesYamlFixtures/customRulesNull.yaml
 var customRulesNull string
 
-func TestCustomRulesNull(t *testing.T) {
+func Test_customRulesNull(t *testing.T) {
 	err := validatePoliciesYaml([]byte(customRulesNull), "./validatePoliciesYamlFixtures/customRulesNull.yaml")
 	assert.Nil(t, err)
+}
+
+//go:embed validatePoliciesYamlFixtures/missingApiVersion.yaml
+var missingApiVersion string
+
+func Test_missingApiVersion(t *testing.T) {
+	err := validatePoliciesYaml([]byte(missingApiVersion), "./validatePoliciesYamlFixtures/missingApiVersion.yaml")
+	assert.EqualError(t, err, "found errors in policies file ./validatePoliciesYamlFixtures/missingApiVersion.yaml:\nmissing properties: 'apiVersion'")
 }
