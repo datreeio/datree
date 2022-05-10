34f883111ebb079897e240d7b20745c1a73587be
Roy Hadad <roy@datree.io>
2022-05-10 17:55:07 +0300

    refactor: fix goimports

--- pkg/policy/validatePoliciesYaml_test.go
+++ pkg/policy/validatePoliciesYaml_test.go
@@ -2,8 +2,9 @@ package policy
 
 import (
 	_ "embed"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 //go:embed validatePoliciesYamlFixtures/customRulesNull.yaml
