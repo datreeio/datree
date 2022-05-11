748d706ccb0edc710c9e40827bcfd5f1f3af1e14
Roy Hadad <39004075+royhadad@users.noreply.github.com>
2022-05-11 13:16:47 +0300

    refactor: move validatePoliciesYaml to a package (#609)
    * refactor: move validatePoliciesYaml to a folder
    
    * fix: add defaultRulesSchema.json back
    
    * refactor: move validatePoliciesYaml into a separate package
    
    * refactor: fix goimports

---
+++ pkg/validatePoliciesYaml/validatePoliciesYaml.go
@@ -0,0 +1,34 @@
+package validatePoliciesYaml
+
+import (
+	_ "embed"
+	"fmt"
+
+	"github.com/datreeio/datree/pkg/jsonSchemaValidator"
+	"github.com/ghodss/yaml"
+)
+
+//go:embed policiesSchema.json
+var policiesSchemaContent string
+
+func ValidatePoliciesYaml(content []byte, policyYamlPath string) error {
+	jsonSchemaValidator := jsonSchemaValidator.New()
+	jsonContent, _ := yaml.YAMLToJSON(content)
+	errorsResult, err := jsonSchemaValidator.Validate(policiesSchemaContent, jsonContent)
+
+	if err != nil {
+		return err
+	}
+
+	if errorsResult != nil {
+		validationErrors := fmt.Errorf("found errors in policies file %s:", policyYamlPath)
+
+		for _, validationError := range errorsResult {
+			validationErrors = fmt.Errorf("%s\n(root)%s: %s", validationErrors, validationError.InstanceLocation, validationError.Error)
+		}
+
+		return validationErrors
+	}
+
+	return nil
+}
