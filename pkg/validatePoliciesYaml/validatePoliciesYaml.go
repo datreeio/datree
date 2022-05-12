7945aef58805cde5addab943c5ca70e41be44b82
Abhra303 <chakrabortyabhradeep79@gmail.com>
2022-05-12 14:10:51 +0530

    feat: add more validation checks for policy file
    When user uses `--policy-config`, datree first validates the policy
    file using a jsonSchema file. But it does not check things like
    identifier uniqueness, single default policy check etc.
    
    Add those check in `validatePoliciesYaml` package and also move
    `defaultRules` specific struct and functions to a dedicated package
    called `defaultRules` for better maintainability.

--- pkg/validatePoliciesYaml/validatePoliciesYaml.go
+++ pkg/validatePoliciesYaml/validatePoliciesYaml.go
@@ -2,10 +2,14 @@ package validatePoliciesYaml
 
 import (
 	_ "embed"
+	"encoding/json"
 	"fmt"
 
+	"github.com/datreeio/datree/pkg/cliClient"
+	"github.com/datreeio/datree/pkg/defaultRules"
 	"github.com/datreeio/datree/pkg/jsonSchemaValidator"
 	"github.com/ghodss/yaml"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 //go:embed policiesSchema.json
@@ -30,5 +34,128 @@ func ValidatePoliciesYaml(content []byte, policyYamlPath string) error {
 		return validationErrors
 	}
 
+	return validatePoliciesContent(jsonContent)
+}
+
+func validatePoliciesContent(content []byte) error {
+	// unmarshal the content
+	var schema *cliClient.EvaluationPrerunPolicies
+	err := json.Unmarshal(content, &schema)
+	if err != nil {
+		return err
+	}
+
+	// validate that exactly one policy is set to default
+	err = validateSingleDefaultPolicy(schema.Policies)
+	if err != nil {
+		return err
+	}
+
+	// validate if the policy file has any identifier related issues
+	err = validateIdentifier(schema.Policies, schema.CustomRules)
+	if err != nil {
+		return err
+	}
+
+	// validate the schema of each rule
+	err = validateSchemaField(schema.CustomRules)
+	return err
+}
+
+func validateIdentifier(policies []*cliClient.Policy, customRules []*cliClient.CustomRule) error {
+
+	err := checkIdentifierUniquenessInPolicy(policies)
+	if err != nil {
+		return err
+	}
+
+	err = checkIdentifierUniqueness(customRules)
+	return err
+}
+
+func checkIdentifierUniquenessInPolicy(policies []*cliClient.Policy) error {
+	for _, policy := range policies {
+		propertyValuesExistenceMap := make(map[string]bool)
+		rules := policy.Rules
+		for _, rule := range rules {
+			identifier := rule.Identifier
+			if propertyValuesExistenceMap[identifier] {
+				return fmt.Errorf("invalid policy file: identifier \"%s\" used atleast two times in policy \"%s\"", identifier, policy.Name)
+			}
+			propertyValuesExistenceMap[identifier] = true
+		}
+	}
+	return nil
+}
+
+func checkIdentifierUniqueness(customRules []*cliClient.CustomRule) error {
+	defaultRules, err := defaultRules.GetDefaultRules()
+	if err != nil {
+		return err
+	}
+	propertyValuesExistenceMap := make(map[string]bool)
+
+	for _, item := range customRules {
+		identifier := item.Identifier
+
+		if propertyValuesExistenceMap[identifier] {
+			return fmt.Errorf("invalid policy file: identifier \"%s\" is used in atleast two rules", identifier)
+		}
+
+		propertyValuesExistenceMap[identifier] = true
+	}
+
+	for _, item := range defaultRules.Rules {
+		identifier := item.UniqueName
+
+		if propertyValuesExistenceMap[identifier] {
+			return fmt.Errorf("invalid policy file: a default rule with same identifier \"%s\" already exists", identifier)
+		}
+		propertyValuesExistenceMap[identifier] = true
+	}
+
+	return nil
+}
+
+func validateSingleDefaultPolicy(policies []*cliClient.Policy) error {
+	sawDefault := false
+	for _, policy := range policies {
+
+		if policy.IsDefault {
+			if sawDefault {
+				return fmt.Errorf("invalid policy file: multiple policies are set to default")
+			}
+			sawDefault = true
+		}
+	}
+	return nil
+}
+
+func validateSchemaField(customRules []*cliClient.CustomRule) error {
+	for _, rule := range customRules {
+		var err error
+		var jsonContent string
+		identifier := rule.Identifier
+		if rule.Schema != nil {
+			var content []byte
+			schema := rule.Schema
+			content, err = json.Marshal(schema)
+			if err != nil {
+				return err
+			}
+			jsonContent = string(content)
+		} else {
+			jsonContent = rule.JsonSchema
+		}
+
+		if jsonContent == "" {
+			return fmt.Errorf("invalid policy file: rule identifier \"%s\" has no associated schema", identifier)
+		}
+		schemaLoader := gojsonschema.NewStringLoader(jsonContent)
+		_, err = gojsonschema.NewSchemaLoader().Compile(schemaLoader)
+		if err != nil {
+			return fmt.Errorf("invalid policy file: rule \"%s\" has schema error: %s", rule.Identifier, err.Error())
+		}
+	}
 	return nil
 }
--- pkg/validatePoliciesYaml/validatePoliciesYaml_test.go
+++ pkg/validatePoliciesYaml/validatePoliciesYaml_test.go
@@ -26,6 +26,12 @@ var missingApiVersion string
 //go:embed test_fixtures/missingPolicyName.yaml
 var missingPolicyName string
 
+//go:embed test_fixtures/missingSchema.yaml
+var missingSchema string
+
+//go:embed test_fixtures/multipleDefaultPolicy.yaml
+var multipleDefaultPolicy string
+
 //go:embed test_fixtures/wrongApiVersion.yaml
 var wrongApiVersion string
 
@@ -41,6 +47,18 @@ var customRuleMissingDefaultMessageOnFailure string
 //go:embed test_fixtures/customRuleValidSchema.yaml
 var customRuleValidSchema string
 
+//go:embed test_fixtures/customRuleInvalidSchema.yaml
+var customRuleInvalidSchema string
+
+//go:embed test_fixtures/customRuleInvalidJsonSchema.yaml
+var customRuleInvalidJsonSchema string
+
+//go:embed test_fixtures/customRuleIdentifierNotUnique1.yaml
+var customRuleIdentifierNotUnique1 string
+
+//go:embed test_fixtures/customRuleIdentifierNotUnique2.yaml
+var customRuleIdentifierNotUnique2 string
+
 func assertValidationResult(t *testing.T, policiesFile string, policiesFilePath string, expectedError error) {
 	err := ValidatePoliciesYaml([]byte(policiesFile), policiesFilePath)
 	assert.Equal(t, err, expectedError)
@@ -53,6 +71,8 @@ func TestValidatePoliciesYaml(t *testing.T) {
 	assertValidationResult(t, missingPolicyRules, "./test_fixtures/missingPolicyRules.yaml", nil)
 	assertValidationResult(t, missingApiVersion, "./test_fixtures/missingApiVersion.yaml", errors.New("found errors in policies file ./test_fixtures/missingApiVersion.yaml:\n(root): missing properties: 'apiVersion'"))
 	assertValidationResult(t, missingPolicyName, "./test_fixtures/missingPolicyName.yaml", errors.New("found errors in policies file ./test_fixtures/missingPolicyName.yaml:\n(root)/policies/0: missing properties: 'name'"))
+	assertValidationResult(t, missingSchema, "./test_fixtures/missingSchema.yaml", errors.New("invalid policy file: rule identifier \"PODDISRUPTIONBUDGET_DENY_ZERO_VOLUNTARY_DISRUPTION\" has no associated schema"))
+	assertValidationResult(t, multipleDefaultPolicy, "./test_fixtures/multipleDefaultPolicy.yaml", errors.New("invalid policy file: multiple policies are set to default"))
 	assertValidationResult(t, wrongApiVersion, "./test_fixtures/wrongApiVersion.yaml", errors.New("found errors in policies file ./test_fixtures/wrongApiVersion.yaml:\n(root)/apiVersion: value must be \"v1\""))
 
 	// customRule
@@ -60,4 +80,8 @@ func TestValidatePoliciesYaml(t *testing.T) {
 	assertValidationResult(t, customRuleMissingName, "./test_fixtures/customRuleMissingName.yaml", errors.New("found errors in policies file ./test_fixtures/customRuleMissingName.yaml:\n(root)/customRules/0: missing properties: 'name'"))
 	assertValidationResult(t, customRuleMissingDefaultMessageOnFailure, "./test_fixtures/customRuleMissingDefaultMessageOnFailure.yaml", errors.New("found errors in policies file ./test_fixtures/customRuleMissingDefaultMessageOnFailure.yaml:\n(root)/customRules/0: missing properties: 'defaultMessageOnFailure'"))
 	assertValidationResult(t, customRuleValidSchema, "./test_fixtures/customRuleValidSchema.yaml", nil)
+	assertValidationResult(t, customRuleInvalidSchema, "./test_fixtures/customRuleInvalidSchema.yaml", errors.New("invalid policy file: rule \"PODDISRUPTIONBUDGET_DENY_ZERO_VOLUNTARY_DISRUPTION\" has schema error: has a primitive type that is NOT VALID -- given: /arrayi/ Expected valid values are:[array boolean integer number null object string]"))
+	assertValidationResult(t, customRuleInvalidJsonSchema, "./test_fixtures/customRuleInvalidJsonSchema.yaml", errors.New("invalid policy file: rule \"PODDISRUPTIONBUDGET_DENY_ZERO_VOLUNTARY_DISRUPTION\" has schema error: Invalid type. Expected: array of schemas, given: definitions"))
+	assertValidationResult(t, customRuleIdentifierNotUnique1, "./test_fixtures/customRuleIdentifierNotUnique1.yaml", errors.New("invalid policy file: identifier \"PODDISRUPTIONBUDGET_DENY_ZERO_VOLUNTARY_DISRUPTION\" is used in atleast two rules"))
+	assertValidationResult(t, customRuleIdentifierNotUnique2, "./test_fixtures/customRuleIdentifierNotUnique2.yaml", errors.New("invalid policy file: identifier \"PODDISRUPTIONBUDGET_DENY_ZERO_VOLUNTARY_DISRUPTION\" used atleast two times in policy \"Default\""))
 }
