d69568c6e84df7ef80147fd52fb34a34a6ddeea2
Dima Brusilovsky <dimabru@gmail.com>
2022-06-01 17:22:38 +0300

    test: deploy version with v

---
+++ scripts/deploy_release_candidate_with_v.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+MAJOR_VERSION=1
+MINOR_VERSION=4
+
+latestRcTag=$(git tag --sort=-version:refname | grep -E "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]+-rc" | head -n 1 | grep --only-matching "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]\+" || true)
+
+if [ "$latestRcTag" == "" ]; then
+    nextVersion=$MAJOR_VERSION.$MINOR_VERSION.0
+else
+    nextVersion=$(echo $latestRcTag | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+fi
+
+export DATREE_BUILD_VERSION=v$nextVersion-test-v
+echo $DATREE_BUILD_VERSION
+
+git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git push --tags
+
+curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
+
+# bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
