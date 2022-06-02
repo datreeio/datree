91486681ebe335087deb56124c73d1d4447f18ce
Dima Brusilovsky <dimabru@gmail.com>
2022-06-02 10:42:09 +0300

    fix: get correct current rc version (#658)

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -4,7 +4,7 @@ set -ex
 MAJOR_VERSION=1
 MINOR_VERSION=4
 
-latestRcTag=$(git tag --sort=-version:refname | grep -E "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]+-rc" | head -n 1 | grep --only-matching "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]\+" || true)
+latestRcTag=$(git tag --sort=-version:refname | grep -E "^v${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]+-rc" | head -n 1 | grep --only-matching "^v${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]\+" || true)
 
 if [ "$latestRcTag" == "" ]; then
     nextVersion=$MAJOR_VERSION.$MINOR_VERSION.0
