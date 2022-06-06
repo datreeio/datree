9afebee72808c414a9a0d91a59a08aab1a85e937
Dima Brusilovsky <dimabru@gmail.com>
2022-06-06 11:43:37 +0300

    build: bump version to 1.5.0

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -2,7 +2,7 @@
 set -ex
 
 MAJOR_VERSION=1
-MINOR_VERSION=4
+MINOR_VERSION=5
 
 latestRcTag=$(git tag --sort=-version:refname | grep -E "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]+-rc" | head -n 1 | grep --only-matching "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]\+" || true)
 
@@ -16,7 +16,6 @@ export DATREE_BUILD_VERSION=$nextVersion-rc
 echo $DATREE_BUILD_VERSION
 
 git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git tag v$DATREE_BUILD_VERSION -a -m "Generated tag with v from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push --tags
 
 curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
