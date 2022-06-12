a3bb8607af927b6c06a78b9066280e271fa829c5
shmuelsa <shmuel@datree.io>
2022-06-12 13:30:16 +0300

    fix: cleanup validate-yaml

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ import (
 	"github.com/datreeio/datree/bl/validation"
 	"github.com/datreeio/datree/cmd"
 	"github.com/datreeio/datree/cmd/test"
-	validateYaml "github.com/datreeio/datree/cmd/validate-yaml"
 )
 
 const DEFAULT_ERR_EXIT_CODE = 1
@@ -70,7 +69,7 @@ func main() {
 	}()
 
 	if err := cmd.Execute(); err != nil {
-		if errors.Is(err, test.ViolationsFoundError) || errors.Is(err, validateYaml.YamlNotValidError) {
+		if errors.Is(err, test.ViolationsFoundError) {
 			os.Exit(VIOLATIONS_FOUND_EXIT_CODE)
 		}
 		reporter.ReportUnexpectedError(err)
