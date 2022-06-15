88530dfd56aa10f63765dc262d2cec1eaed007b7
teselil <tzlil@datree.com>
2022-06-15 13:41:15 +0300

    fix: add rc to release candidate tag

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -12,10 +12,10 @@ else
     nextVersion=$(echo $latestRcTag | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 fi
 
-v_release_tag=v$nextVersion
-
 export DATREE_BUILD_VERSION=$nextVersion-rc
 echo $DATREE_BUILD_VERSION
+
+v_release_tag=v$DATREE_BUILD_VERSION
 
 git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git tag $v_release_tag -a -m "Generated tag with v from TravisCI for build $TRAVIS_BUILD_NUMBER"
