9664a7bcc92751d6ab5fcf9f6baace922547107a
shmu3l <samuelsahala@gmail.com>
2022-09-01 15:43:06 +0300

    fix: printer error line to be json view

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -210,7 +210,7 @@ func (p *Printer) GetWarningsText(warnings []Warning) string {
 				for _, occurrenceDetails := range failedRule.OccurrencesDetails {
 					sb.WriteString(fmt.Sprintf("    - metadata.name: %v (kind: %v)\n", p.getStringOrNotAvailableText(occurrenceDetails.MetadataName), p.getStringOrNotAvailableText(occurrenceDetails.Kind)))
 					for _, validationResult := range occurrenceDetails.ValidationResults {
-						errorPath := fmt.Sprintf("%v (line: %d:%d)\n", validationResult.SchemaPath, validationResult.FailedErrorLine, validationResult.FailedErrorColumn)
+						errorPath := fmt.Sprintf("%v (line: %d:%d)\n", strings.Replace(validationResult.SchemaPath, "/", ".", -1)[1:], validationResult.FailedErrorLine, validationResult.FailedErrorColumn)
 						sb.WriteString(fmt.Sprintf("      > key: %v", errorPath))
 					}
 					sb.WriteString("\n")
