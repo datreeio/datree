186a18a33850091f730a0f1792e4f74774e4ca72
teselil <tzlil@datree.com>
2022-09-12 11:25:07 +0300

    feat: adjust rc flow for gh action

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -17,6 +17,7 @@ echo $DATREE_BUILD_VERSION
 
 v_release_tag=v$DATREE_BUILD_VERSION
 echo $GITHUB_ACTION_RUN_ID
+echo $GITHUB_ACTION_RUN_NUNBER
 #git tag $DATREE_BUILD_VERSION -a -m "Generated tag from GH action for build $GITHUB_ACTION_RUN_ID"
 #git tag $v_release_tag -a -m "Generated tag with v from GH action for build $GITHUB_ACTION_RUN_ID"
 #git push --tags
