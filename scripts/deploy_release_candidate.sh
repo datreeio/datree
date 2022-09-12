de084bf3e0a5dd8b5d118ac0b0a3c38e2d07abd6
TzlilSwimmer123 <51244810+TzlilSwimmer123@users.noreply.github.com>
2022-09-12 16:59:27 +0000

    feat: remove travis cicd impl (#785)
    * feat: support pull request and release candidate ci/cd with github action
    
    Co-authored-by: teselil <tzlil@datree.com>

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -4,6 +4,7 @@ set -ex
 MAJOR_VERSION=1
 MINOR_VERSION=6
 
+git fetch --prune --unshallow --tags
 latestRcTag=$(git tag --sort=-version:refname | grep -E "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]+-rc" | head -n 1 | grep --only-matching "^${MAJOR_VERSION}\.${MINOR_VERSION}.[0-9]\+" || true)
 
 if [ "$latestRcTag" == "" ]; then
@@ -17,10 +18,11 @@ echo $DATREE_BUILD_VERSION
 
 v_release_tag=v$DATREE_BUILD_VERSION
 
-git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git tag $v_release_tag -a -m "Generated tag with v from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git tag $DATREE_BUILD_VERSION -a -m "Generated tag from GH action for build $GITHUB_ACTION_RUN_ID"
+git tag $v_release_tag -a -m "Generated tag with v from GH action for build $GITHUB_ACTION_RUN_ID"
 git push --tags
 
+git restore ./scripts/deploy_release_candidate.sh
 curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
 
 bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
