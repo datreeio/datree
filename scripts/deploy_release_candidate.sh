9eefbdffc29c9baa958e66e22e7b3e972f4228fd
teselil <tzlil@datree.com>
2022-09-12 10:06:07 +0300

    feat: add release candidate w/o actuall release

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -17,10 +17,10 @@ echo $DATREE_BUILD_VERSION
 
 v_release_tag=v$DATREE_BUILD_VERSION
 
-git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git tag $v_release_tag -a -m "Generated tag with v from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git push --tags
-
-curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
-
-bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
+#git tag $DATREE_BUILD_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+#git tag $v_release_tag -a -m "Generated tag with v from TravisCI for build $TRAVIS_BUILD_NUMBER"
+#git push --tags
+#
+#curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
+#
+#bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
