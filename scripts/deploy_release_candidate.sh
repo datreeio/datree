bc8e8f6e26b3fe2d54b1ee3f545d8ab24a1c138a
teselil <tzlil@datree.com>
2022-09-12 12:10:11 +0300

    feat: first rc gh action try

--- scripts/deploy_release_candidate.sh
+++ scripts/deploy_release_candidate.sh
@@ -18,10 +18,10 @@ echo $DATREE_BUILD_VERSION
 
 v_release_tag=v$DATREE_BUILD_VERSION
 
-#git tag $DATREE_BUILD_VERSION -a -m "Generated tag from GH action for build $GITHUB_ACTION_RUN_ID"
-#git tag $v_release_tag -a -m "Generated tag with v from GH action for build $GITHUB_ACTION_RUN_ID"
-#git push --tags
-#
-#curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
-#
-#bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
+git tag $DATREE_BUILD_VERSION -a -m "Generated tag from GH action for build $GITHUB_ACTION_RUN_ID"
+git tag $v_release_tag -a -m "Generated tag with v from GH action for build $GITHUB_ACTION_RUN_ID"
+git push --tags
+
+curl -sL https://git.io/goreleaser | GORELEASER_CURRENT_TAG=$DATREE_BUILD_VERSION GO_BUILD_TAG=staging VERSION=v$GORELEASER_VERSION bash
+
+bash ./scripts/brew_push_formula.sh staging $DATREE_BUILD_VERSION
