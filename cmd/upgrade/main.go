e086f9f44c47a432fadb4decc32278ce32cf83b1
Mustaque Ahmed <amustaque97@gmail.com>
2022-10-14 01:32:42 +0530

    feat: add `upgrade` command in the cmd directory
    this is a wrapper around the main implementation and it plugs-in with
    the cobra command.

---
+++ cmd/upgrade/main.go
@@ -0,0 +1,48 @@
+package upgrade
+
+import (
+	"fmt"
+
+	"github.com/datreeio/datree/pkg/cliClient"
+	"github.com/datreeio/datree/pkg/upgradeManager"
+	"github.com/spf13/cobra"
+)
+
+type Printer interface {
+	PrintMessage(messageText string, messageColor string)
+}
+type UpgradeCommandContext struct {
+	CliVersion       string
+	Printer          Printer
+	UpgradeCliClient *cliClient.CliClient
+}
+
+func upgrade(ctx *UpgradeCommandContext) {
+	fmt.Println(ctx.CliVersion)
+}
+
+func New(ctx *UpgradeCommandContext) *cobra.Command {
+	m := upgrademanager.NewUpgradeManager()
+	var upgradeCmd = &cobra.Command{
+		Use:   "upgrade",
+		Short: "Upgrade datree to the latest version",
+		Long:  "Upgrade datree to the latest version",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) > 0 {
+				ctx.Printer.PrintMessage("Run upgrade command without any argument\n", "error")
+			}
+
+			if m.CheckIfDatreeInstalledUsingBrew() {
+				ctx.Printer.PrintMessage("Looks like you installed Datree via brew, to upgrade datree run - brew upgrade datree\n", "error")
+			}
+
+			err := m.Upgrade()
+			if err != nil {
+				err_msg := fmt.Sprintf("Failed to upgrade datree to the latest version, error encountered %s", err)
+				ctx.Printer.PrintMessage(err_msg, "error")
+			}
+
+		},
+	}
+	return upgradeCmd
+}
