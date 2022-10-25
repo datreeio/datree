867302e84c73e8f0efe7a1b4d1c8d7e41c67d119
Roy Hadad <39004075+royhadad@users.noreply.github.com>
2022-10-25 18:28:38 +0300

    feat: improve suggestions (#821)
    * feat: change kind-not-find suggestion to different text cyan, and only once
    
    * test: fix test
    
    * add tip for non k8s files
    
    * revert fixture file

--- bl/validation/main.go
+++ bl/validation/main.go
@@ -2,7 +2,6 @@ package validation
 
 import (
 	"fmt"
-	"strings"
 )
 
 type InvalidK8sSchemaError struct {
@@ -10,12 +9,5 @@ type InvalidK8sSchemaError struct {
 }
 
 func (e *InvalidK8sSchemaError) Error() string {
-	return fmt.Sprintf("k8s schema validation error: %s\n%s", e.ErrorMessage, e.usageSuggestion())
-}
-
-func (e *InvalidK8sSchemaError) usageSuggestion() string {
-	if strings.HasPrefix(e.ErrorMessage, "could not find schema for ") {
-		return "You can skip files with missing schemas instead of failing by using the `--ignore-missing-schemas` flag\n"
-	}
-	return ""
+	return fmt.Sprintf("k8s schema validation error: %s\n", e.ErrorMessage)
 }
