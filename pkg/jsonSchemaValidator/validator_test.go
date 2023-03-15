36866df23ab301afb1b7e856d096069be11c75a4
teselil <tzlil@datree.com>
2023-03-15 16:41:06 +0200

    add tests to check rego custom key works as expected

--- pkg/jsonSchemaValidator/validator_test.go
+++ pkg/jsonSchemaValidator/validator_test.go
@@ -1,7 +1,14 @@
 package jsonSchemaValidator
 
 import (
+	_ "embed"
+	"encoding/json"
+	"strings"
 	"testing"
+
+	extensions "github.com/datreeio/datree/pkg/jsonSchemaValidator/extensions"
+	"github.com/ghodss/yaml"
+	"github.com/santhosh-tekuri/jsonschema/v5"
 
 	"github.com/datreeio/datree/pkg/fileReader"
 	"github.com/stretchr/testify/assert"
@@ -48,4 +55,77 @@ func TestValidateCustomKeysPass(t *testing.T) {
 	errorsResult, _ := jsonSchemaValidator.ValidateYamlSchema(customRuleSchemaYamlFileContent, passResourceYamlFileContent)
 
 	assert.Empty(t, errorsResult)
+}
+
+//go:embed test_fixtures/invalid-rego-definition.json
+var invalidRegoDefinitionJson string
+
+//go:embed test_fixtures/valid-rego-definition.json
+var validRegoDefinitionJson string
+
+//go:embed test_fixtures/rego-rule-fail.yaml
+var regoRuleFail string
+
+//go:embed test_fixtures/rego-rule-pass.yaml
+var regoRulePass string
+
+func TestRegoDefinitionCustomKey(t *testing.T) {
+	t.Run("invalidSchema", func(t *testing.T) {
+		c := jsonschema.NewCompiler()
+		c.RegisterExtension(extensions.RegoDefinitionCustomKey, extensions.CustomKeyRegoRule, extensions.CustomKeyRegoDefinitionCompiler{})
+		err := c.AddResource("test.json", strings.NewReader(invalidRegoDefinitionJson))
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = c.Compile("test.json")
+		if err == nil {
+			t.Fatal("error expected")
+		}
+		assert.Contains(t, err.Error(), "regoDefinition must be an object of type RegoDefinition json: cannot unmarshal number into Go struct field RegoDefinition.code of type string")
+	})
+	t.Run("validSchema", func(t *testing.T) {
+		c := jsonschema.NewCompiler()
+		c.RegisterExtension(extensions.RegoDefinitionCustomKey, extensions.CustomKeyRegoRule, extensions.CustomKeyRegoDefinitionCompiler{})
+		if err := c.AddResource("test.json", strings.NewReader(validRegoDefinitionJson)); err != nil {
+			t.Fatal(err)
+		}
+		sch, err := c.Compile("test.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Run("validInstance", func(t *testing.T) {
+			jsonYamlContent, err := getInterfaceFromYamlContext(regoRulePass)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if err := sch.Validate(jsonYamlContent); err != nil {
+				t.Fatal(err)
+			}
+		})
+		t.Run("invalidInstance", func(t *testing.T) {
+			jsonYamlContent, err := getInterfaceFromYamlContext(regoRuleFail)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := sch.Validate(jsonYamlContent); err == nil {
+				t.Fatal("validation must fail")
+			} else {
+				t.Logf("%#v", err)
+				if !strings.Contains(err.(*jsonschema.ValidationError).GoString(), "doesn't validate") {
+					t.Fatal("validation error expected to contain regoDefinition message")
+				}
+			}
+		})
+	})
+}
+
+func getInterfaceFromYamlContext(yamlContent string) (interface{}, error) {
+	var jsonYamlContent interface{}
+	regoRuleFailsYamlBytes, _ := yaml.YAMLToJSON([]byte(yamlContent))
+	err := json.Unmarshal(regoRuleFailsYamlBytes, &jsonYamlContent)
+	if err != nil {
+		return nil, err
+	}
+	return jsonYamlContent, nil
 }
