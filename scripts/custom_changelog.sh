3c5592e919673586c67986942e8255757e52349d
Niv Weiss <niv@datree.io>
2023-04-03 16:30:05 +0300

    fix: latestRelease change

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -7,5 +7,6 @@ git pull
 latestCliReleaseFromGithub=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/datreeio/datree/releases/latest")
 # echo $latestCliReleaseFromGithub
 latestRelease=$(echo $latestCliReleaseFromGithub | jq -r '.tag_name' )
-git log --pretty='%h %N %s %n' --no-merges --decorate=full 1.8.47..HEAD > ./changelog.txt
+latestRelease=1.8.47
+git log --pretty='%h %N %s %n' --no-merges --decorate=full ${latestRelease//-rc}..HEAD > ./changelog.txt
 git checkout - 
