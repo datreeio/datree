84819ed5ce63976cdea470b79120aba94f9b5f6b
Niv Weiss <35202955+niv1612@users.noreply.github.com>
2023-04-04 12:27:17 +0300

    ci: logging latest commits and fix release notes (#916)
    * ci: logging latest commits and fixing release notes

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -ex
 
+git rev-parse --abbrev-ref HEAD # Show current branch
+git log -n 5 > latest5commits.txt 
+cat latest5commits.txt 
+
+git checkout main
+git pull
+git log -n 5 > latest5commits.txt
+cat latest5commits.txt
+
 latestCliReleaseFromGithub=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/datreeio/datree/releases/latest")
 # echo $latestCliReleaseFromGithub
 latestRelease=$(echo $latestCliReleaseFromGithub | jq -r '.tag_name' )
 git log --pretty='%h %N %s %n' --no-merges --decorate=full ${latestRelease//-rc}..HEAD > ./changelog.txt
 cat changelog.txt
+git checkout - 
