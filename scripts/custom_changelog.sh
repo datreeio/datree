84206fcae7d42faeea832c2f4aa241e2b29ae39a
Niv Weiss <35202955+niv1612@users.noreply.github.com>
2023-04-04 17:42:59 +0300

    ci: fix release notes issue (#924)

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -ex
 
-git rev-parse --abbrev-ref HEAD # Show current branch
-git status
-git log -n 5 --format="%h" > latest5commits.txt 
-cat latest5commits.txt 
-git log -n 5 > latest5commits.txt 
-cat latest5commits.txt 
 git checkout main
 git pull --unshallow
-git status
-git log -n 5 --format="%h" > latest5commits.txt
-cat latest5commits.txt
 
 latestCliReleaseFromGithub=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/datreeio/datree/releases/latest")
 # echo $latestCliReleaseFromGithub
