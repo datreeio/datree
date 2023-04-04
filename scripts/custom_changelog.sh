4182ad263cdef728164e33531c18feb4c2f832b3
Niv Weiss <niv@datree.io>
2023-04-04 14:13:11 +0300

    ci: add tests for resolving release note issue

--- scripts/custom_changelog.sh
+++ scripts/custom_changelog.sh
@@ -2,12 +2,15 @@
 set -ex
 
 git rev-parse --abbrev-ref HEAD # Show current branch
-git log -n 5 > latest5commits.txt 
+git status
+git log -n 5 --format="%h" > latest5commits.txt 
 cat latest5commits.txt 
 
 git checkout main
 git pull
-git log -n 5 > latest5commits.txt
+git fetch --unshallow
+git status
+git log -n 5 --format="%h" > latest5commits.txt
 cat latest5commits.txt
 
 latestCliReleaseFromGithub=$(curl --silent --header "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/datreeio/datree/releases/latest")
